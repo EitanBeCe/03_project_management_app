a30fd328b796a1e6c9446744e4b9acdcdae19253
Eitan Elberg <eitan.elberg@consumrz.com>
2026-03-16 17:15:37 +0200

    SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import MainSection from './components/MainSection.js'
 import SideBar from './components/SideBar.js'
 
+export interface ProjectCodable {
+  title: string
+}
+
 function App() {
+  const [projects, setProjects] = useState<ProjectCodable[]>([])
+
   return (
-    <main className="flex flex-row">
-      <SideBar />
+    <main className="h-screen flex flex-row">
+      <SideBar projects={projects} setProjects={setProjects}/>
       <MainSection />
     </main>
   )
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,47 @@
-const SideBar = () => {
-  return (
-    <menu className="bg-purple-900 text-white">
-      <p className="text-4xl">YOUR PROJECTS</p>
+import { Dispatch, SetStateAction, useState } from 'react'
+import { ProjectCodable } from '../App.js'
 
-      <button>Add Project</button>
-    </menu>
+interface SideBarProps {
+  projects: ProjectCodable[]
+  setProjects: Dispatch<SetStateAction<ProjectCodable[]>>
+}
+
+const SideBar = ({ projects, setProjects }: SideBarProps) => {
+  const [isTitleError, setIsTitleError] = useState(false)
+
+  const addProject = (newProject: ProjectCodable) => {
+    if (projects.some(p => p.title == newProject.title)) {
+      setIsTitleError(true)
+    } else {
+      setIsTitleError(false)
+
+      setProjects(prev => [...prev, newProject])
+    }
+  }
+
+  return (
+    <aside className="w-1/3 p-4 bg-purple-900 text-white md:w-72 rounded-r-xl">
+      <h2 className="text-3xl my-28 font-bold uppercase">Your projects</h2>
+
+      <button
+        className="text-xl bg-slate-600 p-1 px-4 rounded-md hover:bg-purple-400"
+        onClick={() => addProject({ title: 'Pr 1' })}
+      >
+        + Add Project
+      </button>
+
+      {isTitleError && (
+        <p className="text-red-400 mt-2">
+          You already have a project with that name
+        </p>
+      )}
+
+      <ul className="my-10">
+        {projects.map(p => (
+          <li key={p.title}>{p.title}</li>
+        ))}
+      </ul>
+    </aside>
   )
 }
 
